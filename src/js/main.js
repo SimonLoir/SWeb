6c48d59a328736b869366a8afaf24efcefc4f4c9
SimonLoir <simon-loir@hotmail.com>
2017-12-22 12:20:37 +0100

    updating template chooser

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
+const {
+    dialog
+} = require("electron").remote;
 const app = require('electron').remote;
 const fs = require("fs");
 const os = require('os');
 var sab = require(__dirname + "/../js/sweb-app-builder");
+
 $(document).ready(function () {
     $('#create_empty_project').click(function () {
         $('.project_creation_tool').addClass('visible');
@@ -14,10 +18,31 @@ $(document).ready(function () {
         $('.project_creation_tool').removeClass('visible');
     });
     $('#build_app').click(function () {
-        sab.initProject($('#dir_name').get(0).value, $('#app_name').get(0).value);
+        let done = sab.initProject($('#dir_name').get(0).value, $('#app_name').get(0).value, $('.selected').get(0).getAttribute("data-type"));
+        if(done == true){
+            view.hideStartScreen();
+        }else{
+            console.log("aborded")
+        }
     });
     $(".cross-quit").click(function() {
         var window = app.getCurrentWindow();
         window.close();
     });
+    $("#choose_dir").click(function () {
+        dialog.showOpenDialog({properties: ['openDirectory'], title:"Choisissez un dossier dans lequel mettre votre projet."}, function (f) {
+            if(f== undefined){
+                return;
+            }
+            $('#dir_name').get(0).value = f[0];
+        });
+    });
 });
+
+var view = {
+    hideStartScreen : () => {
+        $('.startscreen').css('display', "none");
+        $('.start').removeClass('start');
+        $('.project_creation_tool').removeClass('visible');        
+    }
+}
--- src/js/sweb-app-builder/main.js
+++ src/js/sweb-app-builder/main.js
@@ -1,7 +1,46 @@
-exports.writeFile = function (filename, data) {
+exports.writeFile = (filename, data) => {
     fs.writeFileSync(filename, data, "utf-8");
 }
 
-exports.initProject = (dir, appname)  => {
-    alert(dir, appname)
+exports.mkdir = (dirname) => {
+    if (fs.existsSync(dirname) != true) {
+        fs.mkdirSync(dirname);
+    }
+}
+
+exports.initProject = (dir, appname, type)  => {
+    let fs = require("fs");
+    if(fs.existsSync(dir) == false){
+        alert("Ce répertoire n'existe pas.");
+        return false;
+    }
+    if(fs.readdirSync(dir).length != 0){
+        alert("Le dossier de destination doit être vide");
+        return false;
+    }
+    let sweb_config = {
+        appname: appname,
+        type:type
+    };
+    try {
+        this.mkdir(dir + "/releases");
+        this.mkdir(dir + "/builds");
+        this.mkdir(dir + "/builds/src");
+        this.mkdir(dir + "/project");
+        this.mkdir(dir + "/project/events");
+        
+        this.writeFile(dir + ".scode.json", '{"project_type":"electron","launch_command" : "electron builds/."}');
+        this.writeFile(dir + "/project/events/index.ready.js", '/* When index.html is ready */ \ndocument.addEventListener("DOMContentLoaded", function () {\n\t/* Some code here */\n\tconsole.log("app is loaded")\n});');
+        this.writeFile(dir + "/project/index.sml", "\n", "utf8");
+        this.writeFile(dir + "/project.sweb", JSON.stringify(sweb_config) , "utf8");
+        this.writeFile(dir + "/project/content.index.sml-content", "{}", "utf8");
+        this.writeFile(dir + "/builds/main.js", fs.readFileSync(__dirname + "/../../resources/base-app.js", "utf-8"));
+        this.writeFile(dir + "/builds/package.json", fs.readFileSync(__dirname + "/../../resources/base-app-packager.json", "utf-8").replace("app-name", appname));
+        this.writeFile(dir + "/builds/src/index.html", fs.readFileSync(__dirname + "/../../resources/base-app.html", "utf-8"));
+    } catch (e) {
+        alert("Erreur lors de la création des dossiers : \n" + e);
+        return false;
+    }
+
+    return true;
 }
